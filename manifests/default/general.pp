3f309e78b833e5fff799f1d59c8c2a0c32a1b353
Hunter Haugen <h.haugen@gmail.com>
2012-10-31 14:03:10 -0700

    Turn general into a defined resource

--- manifests/default/general.pp
+++ manifests/default/general.pp
@@ -1,83 +1,110 @@
 class charybdis::default::general {
-  class { 'charybdis::general':
-    quoted   => {
-      'default_umodes'              => '+i',
-      'oper_snomask'                => '+s',
-      'default_operstring'          => 'is an IRC Operator',
-      'default_adminstring'         => 'is a Server Administrator',
-      'servicestring'               => 'is a Network Service',
-      'identify_service'            => 'NickServ@services.int',
-      'identify_command'            => 'IDENTIFY',
-      'kline_reason'                => 'K-Lined',
-    },
-    unquoted => {
-      'hide_error_messages'         => 'opers',
-      'hide_spoof_ips'              => 'yes',
-      'disable_fake_channels'       => 'no',
-      'tkline_expire_notices'       => 'no',
-      'default_floodcount'          => '10',
-      'failed_oper_notice'          => 'yes',
-      'dots_in_ident'               => '2',
-      'min_nonwildcard'             => '4',
-      'min_nonwildcard_simple'      => '3',
-      'max_accept'                  => '100',
-      'max_monitor'                 => '100',
-      'anti_nick_flood'             => 'yes',
-      'max_nick_time'               => '20 seconds',
-      'max_nick_changes'            => '5',
-      'anti_spam_exit_message_time' => '5 minutes',
-      'ts_warn_delta'               => '30 seconds',
-      'ts_max_delta'                => '5 minutes',
-      'client_exit'                 => 'yes',
-      'collision_fnc'               => 'yes',
-      'global_snotices'             => 'yes',
-      'dline_with_reason'           => 'yes',
-      'kline_delay'                 => '0 seconds',
-      'kline_with_reason'           => 'yes',
-      'non_redundant_klines'        => 'yes',
-      'warn_no_nline'               => 'yes',
-      'use_propagated_bans'         => 'yes',
-      'stats_e_disabled'            => 'no',
-      'stats_c_oper_only'           => 'no',
-      'stats_h_oper_only'           => 'no',
-      'stats_y_oper_only'           => 'no',
-      'stats_o_oper_only'           => 'yes',
-      'stats_P_oper_only'           => 'no',
-      'stats_i_oper_only'           => 'masked',
-      'stats_k_oper_only'           => 'masked',
-      'map_oper_only'               => 'no',
-      'operspy_admin_only'          => 'no',
-      'operspy_dont_care_user_info' => 'no',
-      'caller_id_wait'              => '1 minute',
-      'pace_wait_simple'            => '1 second',
-      'pace_wait'                   => '10 seconds',
-      'short_motd'                  => 'no',
-      'ping_cookie'                 => 'no',
-      'connect_timeout'             => '30 seconds',
-      'default_ident_timeout'       => '5',
-      'disable_auth'                => 'no',
-      'no_oper_flood'               => 'yes',
-      'max_targets'                 => '4',
-      'client_flood'                => '20',
-      'use_whois_actually'          => 'no',
-      'oper_only_umodes'            => [
-        'operwall',
-        'locops',
-        'servnotice',
-      ],
-      'oper_umodes'                 => [
-        'locops',
-        'servnotice',
-        'operwall',
-        'wallop',
-      ],
-      'burst_away'                  => 'yes',
-      'nick_delay'                  => '0 seconds',
-      'reject_ban_time'             => '1 minute',
-      'reject_after_count'          => '3',
-      'reject_duration'             => '5 minutes',
-      'throttle_duration'           => '60',
-      'throttle_count'              => '4',
-    },
+  # Quoted values
+  charybdis::general { 'default_umodes':
+    value  => '+i',
+    quoted => true,
+  }
+  charybdis::general { 'oper_snomask':
+    value  => '+s',
+    quoted => true,
+  }
+  charybdis::general { 'default_operstring':
+    value  => 'is an IRC Operator',
+    quoted => true,
+  }
+  charybdis::general { 'default_adminstring':
+    value  => 'is a Server Administrator',
+    quoted => true,
+  }
+  charybdis::general { 'servicestring':
+    value  => 'is a Network Service',
+    quoted => true,
+  }
+  charybdis::general { 'identify_service':
+    value  => 'NickServ@services.int',
+    quoted => true,
+  }
+  charybdis::general { 'identify_command':
+    value  => 'IDENTIFY',
+    quoted => true,
+  }
+  charybdis::general { 'kline_reason':
+    value  => 'K-Lined',
+    quoted => true,
+  }
+
+  # Unquoted values
+  charybdis::general { 'hide_error_messages'        : value => 'opers', }
+  charybdis::general { 'hide_spoof_ips'             : value => 'yes', }
+  charybdis::general { 'disable_fake_channels'      : value => 'no', }
+  charybdis::general { 'tkline_expire_notices'      : value => 'no', }
+  charybdis::general { 'default_floodcount'         : value => '10', }
+  charybdis::general { 'failed_oper_notice'         : value => 'yes', }
+  charybdis::general { 'dots_in_ident'              : value => '2', }
+  charybdis::general { 'min_nonwildcard'            : value => '4', }
+  charybdis::general { 'min_nonwildcard_simple'     : value => '3', }
+  charybdis::general { 'max_accept'                 : value => '100', }
+  charybdis::general { 'max_monitor'                : value => '100', }
+  charybdis::general { 'anti_nick_flood'            : value => 'yes', }
+  charybdis::general { 'max_nick_time'              : value => '20 seconds', }
+  charybdis::general { 'max_nick_changes'           : value => '5', }
+  charybdis::general { 'anti_spam_exit_message_time': value => '5 minutes', }
+  charybdis::general { 'ts_warn_delta'              : value => '30 seconds', }
+  charybdis::general { 'ts_max_delta'               : value => '5 minutes', }
+  charybdis::general { 'client_exit'                : value => 'yes', }
+  charybdis::general { 'collision_fnc'              : value => 'yes', }
+  charybdis::general { 'global_snotices'            : value => 'yes', }
+  charybdis::general { 'dline_with_reason'          : value => 'yes', }
+  charybdis::general { 'kline_delay'                : value => '0 seconds', }
+  charybdis::general { 'kline_with_reason'          : value => 'yes', }
+  charybdis::general { 'non_redundant_klines'       : value => 'yes', }
+  charybdis::general { 'warn_no_nline'              : value => 'yes', }
+  charybdis::general { 'use_propagated_bans'        : value => 'yes', }
+  charybdis::general { 'stats_e_disabled'           : value => 'no', }
+  charybdis::general { 'stats_c_oper_only'          : value => 'no', }
+  charybdis::general { 'stats_h_oper_only'          : value => 'no', }
+  charybdis::general { 'stats_y_oper_only'          : value => 'no', }
+  charybdis::general { 'stats_o_oper_only'          : value => 'yes', }
+  charybdis::general { 'stats_P_oper_only'          : value => 'no', }
+  charybdis::general { 'stats_i_oper_only'          : value => 'masked', }
+  charybdis::general { 'stats_k_oper_only'          : value => 'masked', }
+  charybdis::general { 'map_oper_only'              : value => 'no', }
+  charybdis::general { 'operspy_admin_only'         : value => 'no', }
+  charybdis::general { 'operspy_dont_care_user_info': value => 'no', }
+  charybdis::general { 'caller_id_wait'             : value => '1 minute', }
+  charybdis::general { 'pace_wait_simple'           : value => '1 second', }
+  charybdis::general { 'pace_wait'                  : value => '10 seconds', }
+  charybdis::general { 'short_motd'                 : value => 'no', }
+  charybdis::general { 'ping_cookie'                : value => 'no', }
+  charybdis::general { 'connect_timeout'            : value => '30 seconds', }
+  charybdis::general { 'default_ident_timeout'      : value => '5', }
+  charybdis::general { 'disable_auth'               : value => 'no', }
+  charybdis::general { 'no_oper_flood'              : value => 'yes', }
+  charybdis::general { 'max_targets'                : value => '4', }
+  charybdis::general { 'client_flood'               : value => '20', }
+  charybdis::general { 'use_whois_actually'         : value => 'no', }
+  charybdis::general { 'burst_away'                 : value => 'yes', }
+  charybdis::general { 'nick_delay'                 : value => '0 seconds', }
+  charybdis::general { 'reject_ban_time'            : value => '1 minute', }
+  charybdis::general { 'reject_after_count'         : value => '3', }
+  charybdis::general { 'reject_duration'            : value => '5 minutes', }
+  charybdis::general { 'throttle_duration'          : value => '60', }
+  charybdis::general { 'throttle_count'             : value => '4', }
+
+  # Array values
+  charybdis::general { 'oper_only_umodes':
+    value => [
+      'operwall',
+      'locops',
+      'servnotice',
+    ],
+  }
+  charybdis::general { 'oper_umodes':
+    value => [
+      'locops',
+      'servnotice',
+      'operwall',
+      'wallop',
+    ],
   }
 }
--- manifests/general.pp
+++ manifests/general.pp
@@ -1,11 +1,25 @@
-class charybdis::general (
-  $quoted   = {},
-  $unquoted = {},
+define charybdis::general (
+  $value,
+  $quoted   = false,
   $conffile = $charybdis::conffile
 ) {
-  concat::fragment { 'general conf':
+  concat::fragment { "${name} general":
     target  => $conffile,
     content => template('charybdis/general.erb'),
     order   => '120',
+  }
+  if ! defined(Concat::Fragment['general open']) {
+    concat::fragment { 'general open':
+      target  => $conffile,
+      content => "general {\n",
+      order   => '119',
+    }
+  }
+  if ! defined(Concat::Fragment['general close']) {
+    concat::fragment { 'general close':
+      target  => $conffile,
+      content => "};\n",
+      order   => '121',
+    }
   }
 }
