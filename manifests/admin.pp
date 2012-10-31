5c8d4d14774cb01ee9f9432feed025a351032418
Hunter Haugen <h.haugen@gmail.com>
2012-10-31 12:31:45 -0700

    Fix dynamic lookup

--- manifests/admin.pp
+++ manifests/admin.pp
@@ -1,7 +1,8 @@
 class charybdis::admin (
   $adminname    = "Lazy Admin",
   $description  = "IRC network admin",
-  $email        = "admin@127.0.0.1"
+  $email        = "admin@127.0.0.1",
+  $conffile     = $charybdis::conffile
 ) {
   concat::fragment { 'admin conf':
     target  => $conffile,
