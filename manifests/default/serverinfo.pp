9a084b3f5ebda162d953d6133fa11fd7070cec95
Hunter Haugen <h.haugen@gmail.com>
2012-10-30 23:27:16 -0700

    Add ssl key generation

--- manifests/default/serverinfo.pp
+++ manifests/default/serverinfo.pp
@@ -1,6 +1,9 @@
 class charybdis::default::serverinfo {
   class { 'charybdis::serverinfo':
-    restartpass => 'changeme',
-    diepass     => 'changeme',
+    restartpass     => 'changeme',
+    diepass         => 'changeme',
+    ssl_private_key => '/etc/charybdis/test.key',
+    ssl_cert        => '/etc/charybdis/test.cert',
+    ssl_dh_params   => '/etc/charybdis/dh.pem',
   }
 }
--- manifests/serverinfo.pp
+++ manifests/serverinfo.pp
@@ -22,6 +22,51 @@ class charybdis::serverinfo (
     false => 'no',
   }
 
+  Exec {
+    logoutput => 'on_failure',
+    path      => [
+      '/bin',
+      '/sbin',
+      '/usr/bin',
+      '/usr/sbin',
+    ],
+  }
+  if $ssl_private_key and $ssl_cert {
+    exec { 'Create private key and cert':
+      command => "openssl req -x509 -nodes -newkey rsa:1024 -keyout ${ssl_private_key} -out ${ssl_cert} -subj '/C=US/ST=Oregon/L=Portland/O=PuppetLabs/CN=${server_name}'",
+      creates => $ssl_private_key,
+      require => Package['charybdis'],
+      before  => [
+        Concat[$conffile],
+        File[$ssl_private_key],
+        File[$ssl_cert],
+      ],
+    }
+    file { $ssl_private_key:
+      owner => 'root',
+      group => 'charybdis',
+    }
+    file { $ssl_cert:
+      owner => 'root',
+      group => 'charybdis',
+    }
+  }
+  if $ssl_dh_params {
+    exec { 'Create dhparam':
+      command => "openssl dhparam -out $ssl_dh_params 1024",
+      creates => $ssl_dh_params,
+      require => Package['charybdis'],
+      before  => [
+        Concat[$conffile],
+        File[$ssl_dh_params],
+      ],
+    }
+    file { $ssl_dh_params:
+      owner => 'root',
+      group => 'charybdis',
+    }
+  }
+
   concat::fragment { 'serverinfo conf':
     target  => $conffile,
     content => template('charybdis/serverinfo.erb'),
